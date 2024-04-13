0093f614e1547ed3a949dc82608dd15ab4059313
Luis Masuelli <luisfmasuelli@gmail.com>
2024-04-12 22:37:42 -0500

    Adding another comment.

--- Assets/Scripts/Models/CharacterRefreshData.cs
+++ Assets/Scripts/Models/CharacterRefreshData.cs
@@ -2,6 +2,10 @@ using AlephVault.Unity.Binary;
 
 namespace Models
 {
+    /// <summary>
+    ///   This is the refresh message for the characters.
+    ///   It only adds serialization.
+    /// </summary>
     public class CharacterRefreshData : CharacterCommonData
     {
         public override void Serialize(Serializer serializer)
